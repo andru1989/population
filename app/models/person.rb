96b8260c3ea407fe2a30507e70c4cdcd5817b966
Andres Felipe Vizcaino Restrepo <andruvizcaino1989@gmail.com>
2015-09-22 11:50:06 -0500

    validations for person added, views to create a person modified

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,16 @@
 class Person < ActiveRecord::Base
   belongs_to :city
+
+  validates :identification_type, presence: true
+  validates :identification,      presence: true
+  validates :name,                presence: true
+  validates :last_name,           presence: true
+  validates :city_id,             presence: true
+
+  enum identification_type: [:identification_card, :passport]
+  enum gender: [:male, :female]
+
+  def full_name
+    [name, last_name].join(' ')
+  end
 end
