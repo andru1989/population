93ccb8cfd62f457c1cb49a7665979bae2521d020
Andres Felipe Vizcaino Restrepo <andruvizcaino1989@gmail.com>
2015-09-22 12:15:59 -0500

    views for person creation modified, gem for decorators added

---
+++ app/decorators/person_decorator.rb
@@ -0,0 +1,15 @@
+module PersonDecorator
+
+  def select_identification_types
+    Person.identification_types.map do |name, _|
+      [name.humanize, name]
+    end
+  end
+
+  def select_genders
+    Person.genders.map do |name, _|
+      [name.humanize, name]
+    end
+  end
+
+end
