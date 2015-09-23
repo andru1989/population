5dd9d96c46c2ec43d428f371b05c3adfe7f01614
Andres Felipe Vizcaino Restrepo <andruvizcaino1989@gmail.com>
2015-09-23 14:46:44 -0500

    scenario to try to create a person with duplicate identification added

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 feature 'Person' do
 
   scenario 'adds a new person' do
-    expect{
-      person = build(:person)
-      visit people_path
-      click_link 'New Person'
-
-      fill_fields(person)
-
-      click_button 'Create Person'
-    }.to change(Person, :count).by(1)
+    expect{ create_person }.to change(Person, :count).by(1)
 
     expect(current_path).to eq people_path
     expect(page).to have_content 'Person was successfully created.'
 
+  end
+
+  scenario 'try to add a person with a duplicate identification' do
+    person = build(:person, identification: '10373737')
+
+    2.times do
+      expect(create_person(person))
+    end
+
+    expect(page).to have_content 'Identification has already been taken'
   end
 
   scenario 'edits a person' do
