ff3015c0097b99f0e29894caa2875ff871c4299d
Andres Felipe Vizcaino Restrepo <andruvizcaino1989@gmail.com>
2015-09-24 16:46:36 -0500

    file migration to enable hstore and creation of field in people table

--- db/migrate/20150924200313_add_column_to_people.rb
+++ db/migrate/20150924200313_add_column_to_people.rb
@@ -2,5 +2,6 @@ class AddColumnToPeople < ActiveRecord::Migration
   def change
     enable_extension "hstore"
     add_column :people, :work_experience, :hstore
+    add_index :people, :work_experience, using: :gin
   end
 end
--- db/migrate/20150924200348_index_people_work_experience.rb
+++
@@ -1,9 +0,0 @@
-class IndexPeopleWorkExperience < ActiveRecord::Migration
-  def up
-    execute "CREATE INDEX people_work_experience ON people USING GIN(work_experience)"
-  end
-
-  def down
-    execute "DROP INDEX people_work_experience"
-  end
-end
